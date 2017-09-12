e23376a8f89edceda3df71738b03e67959bd7e6c
Amelie <a.pioger@outlook.fr>
2017-09-12 09:49:16 +0200

    finish commit

--- entete.php
+++ entete.php
@@ -1,4 +1,5 @@
 
+
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-3"></h1>
@@ -7,7 +8,8 @@
 </div>
 <div>
 <br>
+<div class id="hello">
 
 <a href="bonjour.php?nom=Dupont&amp;prenom=Jean">Dis-moi bonjour !</a>
-
+</div>
 </div>
